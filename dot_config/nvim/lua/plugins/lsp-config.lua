6cb1b71f73fe1ba4ea1c3daed7400a68d49fbeb3
Hoon Wee <mrgravity817@gmail.com>
2024-01-21 17:35:28 +0900

    nvim: add telescope ui select

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -17,8 +17,14 @@ return {
     "neovim/nvim-lspconfig",
     name = "lsconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      local config = require("lspconfig")
+      config.lua_ls.setup({})
+      -- keymaps
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
     end
   }
 }
