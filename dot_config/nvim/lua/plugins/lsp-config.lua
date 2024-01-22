2a1dc725cc18980fa9685778a5776a248d35cdba
Hoon Wee <mrgravity817@gmail.com>
2024-01-22 21:21:08 +0900

    nvim: enable completion for elixir

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -1,43 +1,43 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-			-- keymaps
-			vim.keymap.set("n", "<leader>cm", ":Mason<CR>", {})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"tsserver",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		name = "lsconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+      -- keymaps
+      vim.keymap.set("n", "<leader>cm", ":Mason<CR>", {})
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    name = "lsconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local config = require("lspconfig")
-			config.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			config.tsserver.setup({
-				capabilities = capabilities,
-			})
-			-- keymaps
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-			vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
-		end,
-	},
+      local config = require("lspconfig")
+      config.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      config.tsserver.setup({
+        capabilities = capabilities,
+      })
+      -- keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
+    end,
+  },
 }
