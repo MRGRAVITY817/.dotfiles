183051eb0fdf6d48c7a2e82afde57481b7a7df57
Hoon Wee <mrgravity817@gmail.com>
2024-01-23 12:45:17 +0900

    nvim: add diagnostics

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,37 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = false,
-			ensure_installed = {
-				"lua",
-				"javascript",
-				"typescript",
-				"tsx",
-				"rust",
-				"toml",
-				"yaml",
-				"json",
-				"elixir",
-				"heex",
-				"eex",
-				"markdown",
-				"fennel",
-				"clojure",
-				"scheme",
-				"racket",
-			},
-			ignore_install = {
-				"dart",
-				"swift",
-			},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = false,
+      ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+        "rust",
+        "toml",
+        "yaml",
+        "json",
+        "elixir",
+        "heex",
+        "eex",
+        "markdown",
+        "fennel",
+        "clojure",
+        "scheme",
+        "racket",
+      },
+      ignore_install = {
+        "dart",
+        "swift",
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+    })
+  end,
 }
--- dot_config/nvim/lua/vim-options.lua
+++ dot_config/nvim/lua/vim-options.lua
@@ -10,7 +10,7 @@ vim.keymap.set("n", "<leader>-", "<C-w>s", {})
 
 -- Navigating panes
 for _, arrow_key in ipairs({ "j", "k", "l", "h" }) do
-	vim.keymap.set("n", string.format("<C-%s>", arrow_key), string.format("<C-w>%s", arrow_key), {})
+  vim.keymap.set("n", string.format("<C-%s>", arrow_key), string.format("<C-w>%s", arrow_key), {})
 end
 
 -- Open terminal in pane below
@@ -22,3 +22,15 @@ vim.cmd("set clipboard+=unnamedplus")
 
 -- Auto format
 vim.cmd("autocmd BufWritePre * lua vim.lsp.buf.format({ async = false })")
+
+-- Diagnostics
+vim.keymap.set("n", "<leader>sd", ":lua vim.diagnostic.open_float()<CR>", {})
+vim.keymap.set("n", "<leader>dd", ":Telescope diagnostics<CR>", {})
+vim.keymap.set("n", "]h", ":lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.HINT })<CR>", {})
+vim.keymap.set("n", "[h", ":lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.HINT })<CR>", {})
+vim.keymap.set("n", "]i", ":lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.INFO })<CR>", {})
+vim.keymap.set("n", "[i", ":lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.INFO })<CR>", {})
+vim.keymap.set("n", "]w", ":lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })<CR>", {})
+vim.keymap.set("n", "[w", ":lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })<CR>", {})
+vim.keymap.set("n", "]e", ":lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })<CR>", {})
+vim.keymap.set("n", "[e", ":lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })<CR>", {})
