a875baeb1ced93b80dcf0046628771df1d7b9787
Hoon Wee <mrgravity817@gmail.com>
2024-02-07 13:05:07 +0900

    nvim: remove ui select

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,43 +1,15 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		name = "telescope",
-		config = function()
-			local builtin = require("telescope.builtin")
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    name = "telescope",
+    config = function()
+      local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
-
-			-- Override mappings
-			local actions = require("telescope.actions")
-			require("telescope").setup({
-				defaults = {
-					mappings = {
-						i = {
-							["jk"] = { "<esc>", type = "command" },
-						},
-					},
-				},
-			})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			-- UI select
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({
-							-- even more opts
-						}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+      vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
+    end,
+  },
 }
