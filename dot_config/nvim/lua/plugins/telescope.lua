26aafa4961e4264d10f1aa3b818c54982216f0ec
Hoon Wee <mrgravity817@gmail.com>
2024-02-07 10:45:55 +0900

    nvim: lsp reference with telescope

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -11,4 +11,20 @@ return {
 			vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
 		end,
 	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			-- UI select
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({
+							-- even more opts
+						}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
