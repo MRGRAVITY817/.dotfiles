ccf88a478f35a402e281ddab2529cc2235dfb65d
Hoon Wee <mrgravity817@gmail.com>
2024-02-08 16:19:04 +0900

    nvim: configure focus mode plugins

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -1,26 +1,29 @@
 return {
-	"epwalsh/obsidian.nvim",
-	version = "*",
-	lazy = true,
-	ft = "markdown",
-	dependencies = {
-		-- Required.
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"preservim/vim-markdown",
-	},
-	opts = {
-		workspaces = {
-			{
-				name = "personal",
-				path = "~/vaults/personal",
-			},
-			{
-				name = "work",
-				path = "~/vaults/work",
-			},
-		},
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "preservim/vim-markdown",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/vaults/personal",
+      },
+      {
+        name = "work",
+        path = "~/vaults/work",
+      },
+    },
 
-		-- see below for full list of options 👇
-	},
+    -- see below for full list of options 👇
+    ui = {
+      enable = false,
+    },
+  },
 }
