328fb6fce67d82e2dc4aa037d3f89c805e1035a6
Hoon Wee <mrgravity817@gmail.com>
2024-02-24 23:01:23 +0900

    nvim: use autotag from the plugin manager

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,59 +1,49 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = false,
-			ensure_installed = {
-				"lua",
-				"javascript",
-				"typescript",
-				"tsx",
-				"rust",
-				"toml",
-				"yaml",
-				"json",
-				"elixir",
-				"heex",
-				"eex",
-				"markdown",
-				"fennel",
-				"clojure",
-				"scheme",
-				"racket",
-			},
-			ignore_install = {
-				"dart",
-				"swift",
-			},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-			autotag = {
-				enable = true,
-				filetypes = {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = false,
+				ensure_installed = {
+					"lua",
 					"html",
 					"javascript",
 					"typescript",
-					"javascriptreact",
-					"typescriptreact",
 					"svelte",
 					"vue",
 					"tsx",
-					"jsx",
-					"rescript",
 					"xml",
-					"php",
 					"markdown",
 					"astro",
-					"glimmer",
-					"handlebars",
-					"hbs",
+					"rust",
+					"toml",
+					"yaml",
+					"json",
+					"elixir",
+					"heex",
+					"eex",
+					"markdown",
+					"fennel",
+					"clojure",
+					"scheme",
+					"racket",
 				},
-			},
-		})
-	end,
+				ignore_install = {
+					"dart",
+					"swift",
+				},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		opts = {},
+	},
 }
