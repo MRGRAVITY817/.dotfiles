65ed257147ed2719fdb3f7d814d91ef6e7a4dbe0
Hoon Wee <mrgravity817@gmail.com>
2024-02-27 10:39:04 +0900

    nvim: open recent files from current git workspace

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -5,12 +5,23 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     name = "telescope",
     config = function()
+      local function get_git_dir()
+        local git_dir = vim.fn.trim(vim.fn.system("git rev-parse --show-toplevel"))
+        return git_dir
+      end
+
       local builtin = require("telescope.builtin")
 
       vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
-      vim.keymap.set("n", "<leader>fj", builtin.buffers, {})
+      -- Show recent files in the current git directory
+      vim.keymap.set("n", "<leader>fj", function()
+        builtin.oldfiles({
+          cwd = get_git_dir(),
+        })
+      end, {})
+      vim.keymap.set("n", "<leader>bf", builtin.buffers, {})
     end,
   },
 }
