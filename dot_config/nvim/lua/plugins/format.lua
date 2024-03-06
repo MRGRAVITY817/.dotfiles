3d0a75ceb5f03a24d01d5b75f55122c5b58a4482
Hoon Wee <mrgravity817@gmail.com>
2024-03-06 10:20:58 +0900

    nvim: remove none-ls and use nvim-lint instead

--- dot_config/nvim/lua/plugins/format.lua
+++ dot_config/nvim/lua/plugins/format.lua
@@ -1,15 +1,29 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				python = { "isort", "black" },
-				lua = { "stylua" },
-				javascript = { "prettier", "prettierd" },
-				elixir = { "mix" },
-				clojure = { "cljstyle" },
-			},
-		})
-	end,
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					python = { "isort", "black" },
+					lua = { "stylua" },
+					javascript = { "prettier", "prettierd" },
+					elixir = { "mix" },
+					clojure = { "cljstyle" },
+				},
+			})
+		end,
+	},
+	{
+		"mfussenegger/nvim-lint",
+		optional = true,
+		opts = function(_, opts)
+			if vim.fn.executable("credo") == 0 then
+				return
+			end
+			opts.linters_by_ft = {
+				elixir = { "credo" },
+			}
+		end,
+	},
 }
