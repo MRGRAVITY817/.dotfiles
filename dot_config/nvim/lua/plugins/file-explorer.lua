0b0c4dc3d84e6591395e7ab33301a0cce15c932b
Hoon Wee <mrgravity817@gmail.com>
2024-05-19 23:37:15 +0900

    nvim and tmux sync

--- dot_config/nvim/lua/plugins/file-explorer.lua
+++ dot_config/nvim/lua/plugins/file-explorer.lua
@@ -9,7 +9,26 @@ return {
 			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
 		config = function()
-			vim.keymap.set("n", "<leader>fe", ":Neotree filesystem toggle left<CR>", {})
+			vim.keymap.set("n", "<leader>fe", function()
+				local reveal_file = vim.fn.expand("%:p")
+				if reveal_file == "" then
+					reveal_file = vim.fn.getcwd()
+				else
+					local f = io.open(reveal_file, "r")
+					if f then
+						f.close(f)
+					else
+						reveal_file = vim.fn.getcwd()
+					end
+				end
+				require("neo-tree.command").execute({
+					action = "focus", -- OPTIONAL, this is the default value
+					source = "filesystem", -- OPTIONAL, this is the default value
+					position = "left", -- OPTIONAL, this is the default value
+					reveal_file = reveal_file, -- path to file or folder to reveal
+					reveal_force_cwd = true, -- change cwd without asking if needed
+				})
+			end, { desc = "Open neo-tree at current file or working directory" })
 
 			require("neo-tree").setup({
 				filesystem = {
