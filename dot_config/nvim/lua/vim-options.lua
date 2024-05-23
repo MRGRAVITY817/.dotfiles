6661ebf5391774d5746c9bac98bec6bedb3d8886
Hoon Wee <mrgravity817@gmail.com>
2024-05-23 23:23:58 +0900

    nvim: escape terminal window

--- dot_config/nvim/lua/vim-options.lua
+++ dot_config/nvim/lua/vim-options.lua
@@ -31,6 +31,20 @@ end
 vim.keymap.set("n", "<leader>ft", ":split | wincmd j | term<CR>i", {})
 vim.cmd("autocmd TermClose * exe 'bdelete! '..expand('<abuf>')")
 
+-- To use ALT+{h,j,k,l} to navigate windows from any mode:
+vim.keymap.set("t", "<A-h>", "<C-\\><C-N><C-w>h", {})
+vim.keymap.set("t", "<A-j>", "<C-\\><C-N><C-w>j", {})
+vim.keymap.set("t", "<A-k>", "<C-\\><C-N><C-w>k", {})
+vim.keymap.set("t", "<A-l>", "<C-\\><C-N><C-w>l", {})
+vim.keymap.set("i", "<A-h>", "<C-\\><C-N><C-w>h", {})
+vim.keymap.set("i", "<A-j>", "<C-\\><C-N><C-w>j", {})
+vim.keymap.set("i", "<A-k>", "<C-\\><C-N><C-w>k", {})
+vim.keymap.set("i", "<A-l>", "<C-\\><C-N><C-w>l", {})
+vim.keymap.set("n", "<A-h>", "<C-w>h", {})
+vim.keymap.set("n", "<A-j>", "<C-w>j", {})
+vim.keymap.set("n", "<A-k>", "<C-w>k", {})
+vim.keymap.set("n", "<A-l>", "<C-w>l", {})
+
 -- Copy/Cut/Paste Clipboard
 vim.cmd("set clipboard+=unnamedplus")
 
