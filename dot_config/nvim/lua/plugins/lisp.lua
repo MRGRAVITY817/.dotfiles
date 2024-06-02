09f6a096a883c0283bc1d7bd846eba9246eec1f7
Hoon Wee <mrgravity817@gmail.com>
2024-06-03 00:57:37 +0900

    nvim: ensure common lisp is installed

--- dot_config/nvim/lua/plugins/lisp.lua
+++ dot_config/nvim/lua/plugins/lisp.lua
@@ -49,8 +49,38 @@ return {
 	{
 		"julienvincent/nvim-paredit",
 		config = function()
-			require("nvim-paredit").setup({
-				filetypes = { "clojure", "cljd" },
+			local paredit = require("nvim-paredit")
+
+			paredit.setup({
+				filetypes = { "clojure", "cljd", "commonlisp" },
+				extensions = {
+					commonlisp = {
+						-- Should return the 'root' of the given Treesitter node. For example:
+						-- The node at cursor in the below example is `()` or 'list_lit':
+						--   '(|)
+						-- But the node root is `'()` or 'quoting_lit'
+						get_node_root = function(node) end,
+						-- This is the inverse of `get_node_root` for forms and should find the inner node for which
+						-- the forms elements are direct children.
+						--
+						-- For example given the node `'()` or 'quoting_lit', this function should return `()` or 'list_lit'.
+						unwrap_form = function(node) end,
+						-- Accepts a Treesitter node and should return true or false depending on whether the given node
+						-- can be considered a 'form'
+						node_is_form = function(node) end,
+						-- Accepts a Treesitter node and should return true or false depending on whether the given node
+						-- can be considered a 'comment'
+						node_is_comment = function(node) end,
+						-- Accepts a Treesitter node representing a form and should return the 'edges' of the node. This
+						-- includes the node text and the range covered by the node
+						get_form_edges = function(node)
+							return {
+								left = { text = "#{", range = { 0, 0, 0, 2 } },
+								right = { text = "}", range = { 0, 5, 0, 6 } },
+							}
+						end,
+					},
+				},
 			})
 		end,
 	},
--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -9,6 +9,7 @@ return {
 				ensure_installed = {
 					"astro",
 					"clojure",
+					"commonlisp",
 					"eex",
 					"elixir",
 					"fennel",
